73d9bcf86c14bb126e1417c4199ffe2390264623
Medfellow Academy LLP <medfellowacademy@gmail.com>
2026-08-26 12:29:53 +0530

    Update page.tsx

--- app/apply/page.tsx
+++ app/apply/page.tsx
@@ -11,6 +11,7 @@ const PROGRAMS = [
   { value: 'internal-medicine', label: 'Fellowship in Internal Medicine' },
   { value: 'family-medicine', label: 'Fellowship in Family Medicine' },
   { value: 'Infectious Disease', label: 'Fellowship in Infectious Disease' },
+  { value: ' Palliative Medicine & Supportive Care', label: 'Fellowship Palliative Medicine & Supportive Care' },
   // Cardiology
   { value: 'clinical-cardiology', label: 'Fellowship in Clinical Cardiology' },
   { value: 'echocardiography', label: 'Fellowship in Echocardiography' },
